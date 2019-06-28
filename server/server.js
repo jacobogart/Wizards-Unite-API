d039e6cdf92832fbf91e72e26e19c9fd6721f10a
jacobogart <jacobogart@gmail.com>
2019-06-28 09:40:14 -0600

    Update port again

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const app = require('./app.js');
 
-app.set('port', process.env.PORT || 3000);
+const port = process.env.PORT || 3000
+
+app.set('port', port);
 
 app.listen(3000, () => {
-  console.log('Express server is running on localhost:3000')
+  console.log(`Express server is running on ${port}`)
 })
